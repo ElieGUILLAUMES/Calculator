04942d6c477195f082b2dbfc26f9d5d75a0e2f49
AndreasPreg <andreaspreg@gmail.com>
2015-08-25 11:30:50 +0000

    Programm logic added

--- app/src/main/java/com/icelandic_courses/elie/calculator/MainActivity.java
+++ app/src/main/java/com/icelandic_courses/elie/calculator/MainActivity.java
@@ -2,8 +2,9 @@ package com.icelandic_courses.elie.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends Activity {
 
@@ -33,5 +34,102 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Append a number
+     * @param view
+     */
+    public void appendNumber(View view) {
+        Button b = (Button) view;
+        TextView tv = (TextView) findViewById(R.id.resultTextView);
+        tv.append(b.getText());
+    }
+
+    /**
+     * Append an operator and validate it
+     * @param view
+     */
+    public void appendOperator(View view) {
+        Button b = (Button) view;
+        TextView tv = (TextView) findViewById(R.id.resultTextView);
+
+        String code = tv.getText().toString();
+        String operator = b.getText().toString();
+
+        //check for operator at end and remove it
+        if(code.length() != 0) {
+            char lastCodeChar = code.charAt(code.length() - 1);
+            if(lastCodeChar == '+' || lastCodeChar == '-') {
+                code = code.subSequence(0, code.length() - 1).toString();
+            }
+        }
+
+        //append operator
+        code = code + operator;
+
+        //apply it
+        tv.setText(code);
+    }
+
+    /**
+     * Remove the last character of the text view
+     * @param view
+     */
+    public void backspace(View view) {
+        TextView tv = (TextView) findViewById(R.id.resultTextView);
+        CharSequence text = tv.getText();
+        text = text.subSequence(0, Math.max(text.length()-1, 0));
+        tv.setText(text);
+    }
+
+    /**
+     * Clear the text view
+     * @param view
+     */
+    public void clear(View view) {
+        TextView tv = (TextView) findViewById(R.id.resultTextView);
+        tv.setText("");
+    }
+
+    /**
+     * Calculate the result
+     * @param view
+     */
+    public void result(View view) {
+        TextView tv = (TextView) findViewById(R.id.resultTextView);
+
+        // add 0 to be able to calculate "-300+100" as "0-300+100"
+        CharSequence code = "0" + tv.getText();
+
+        int i = 0;
+        int result = 0;
+        int currentValue = 0;
+        char lastOperator = '+';
+
+        do {
+            char currentChar = code.charAt(i++);
+            if(currentChar >= '0' && currentChar <= '9') {
+                int currentNumber = Character.getNumericValue(currentChar);
+                currentValue *= 10;
+                currentValue += currentNumber;
+            }
+            else if(currentChar == '+' || currentChar == '-') {
+                if(lastOperator == '+') {
+                    result += currentValue;
+                    currentValue = 0;
+                }
+                else if(lastOperator == '-') {
+                    result -= currentValue;
+                    currentValue = 0;
+                }
+                lastOperator = currentChar;
+            }
+        } while(i < code.length());
+
+        result = result + (lastOperator == '+' ? 1 : -1) * currentValue;
+
+        //apply result value
+        tv.setText(String.valueOf(result));
     }
 }
